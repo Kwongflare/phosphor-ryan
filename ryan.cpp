304ba99c9455534be5e79725b93613696ee50ab1
ryan <ryan.chow@utexas.edu>
2020-07-23 12:51:13 -0500

    Fixing property name

--- ryan.cpp
+++ ryan.cpp
@@ -34,7 +34,7 @@ int main( int argc, char *argv[] )
 
     printf("%d\n", argc);
     
-    m.append("xyz.openbmc_project.State.Chassis", "CurrentPowerStatus");
+    m.append("xyz.openbmc_project.State.Chassis", "CurrentPowerState");
     
     //   auto m =
     //       b.new_method_call("org.freedesktop.DBus", "/org/freedesktop/DBus",
